e5f7de99491e0e0d3dce0116b60065f70682093a
farkhutdinova <evg.farkhutdinova@gmail.com>
2024-06-01 11:18:13 +0200

    created project

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CloneNetcat;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            ShowNetcat();
+            return;
+        }
+
+        if (args.Length < 4)
+        {
+            Console.WriteLine("Usage: ccnc -l -p <port>");
+        }
+
+        var tool = args[0];
+        var command = args[1];
+        var key1 = args[2];
+        var value1 = args[3];
+    }
+
+    private static void ShowNetcat()
+    {
+        const string netcatString = """
+
+                                    ...   ..  .......  ........  .......      ...      ........
+                                    ....  ..  ..          ..     ..          .. ..        ..
+                                    .. .. ..  .....       ..     ..         ..   ..       ..
+                                    ..  ....  ..          ..     ..        .........      ..
+                                    ..   ...  .......     ..     .......  ..       ..     ..
+
+                                    """;
+        Console.WriteLine(netcatString);
+    }
+}
