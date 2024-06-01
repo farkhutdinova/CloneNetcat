3122aaf83c62aa52b97b63ff7e262b31bb03d823
farkhutdinova <evg.farkhutdinova@gmail.com>
2024-06-01 12:24:37 +0200

    created TcpServer

--- Program.cs
+++ Program.cs
@@ -1,37 +1,7 @@
-﻿namespace CloneNetcat;
+using CloneNetcat;
 
-internal static class Program
-{
-    private static void Main(string[] args)
-    {
-        if (args.Length == 0)
-        {
-            ShowNetcat();
-            return;
-        }
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
 
-        if (args.Length < 4)
-        {
-            Console.WriteLine("Usage: ccnc -l -p <port>");
-        }
-
-        var tool = args[0];
-        var command = args[1];
-        var key1 = args[2];
-        var value1 = args[3];
-    }
-
-    private static void ShowNetcat()
-    {
-        const string netcatString = """
-
-                                    ...   ..  .......  ........  .......      ...      ........
-                                    ....  ..  ..          ..     ..          .. ..        ..
-                                    .. .. ..  .....       ..     ..         ..   ..       ..
-                                    ..  ....  ..          ..     ..        .........      ..
-                                    ..   ...  .......     ..     .......  ..       ..     ..
-
-                                    """;
-        Console.WriteLine(netcatString);
-    }
-}
+var host = builder.Build();
+host.Run();
---
+++ TcpServer.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CloneNetcat;
+
+internal sealed class TcpServer
+{
+    public TcpServer()
+    {
+        ShowNetcat();
+        StartServer();
+    }
+    
+    private static void ShowNetcat()
+    {
+        const string netcatString = """
+
+                                    ...   ..  .......  ........  .......      ...      ........
+                                    ....  ..  ..          ..     ..          .. ..        ..
+                                    .. .. ..  .....       ..     ..         ..   ..       ..
+                                    ..  ....  ..          ..     ..        .........      ..
+                                    ..   ...  .......     ..     .......  ..       ..     ..
+
+                                    """;
+        Console.WriteLine(netcatString);
+    }
+
+    private void StartServer()
+    {
+        var port = 8888;
+        var hostAddress = IPAddress.Parse("127.0.0.1");
+        var listener = new TcpListener(hostAddress, port);
+        listener.Start();
+
+        using var client = listener.AcceptTcpClient();
+
+        var buffer = new byte[256];
+        var stream = client.GetStream();
+
+        while (stream.Read(buffer, 0, buffer.Length) != 0)
+        {
+            var bytesRead = stream.Read(buffer, 0, buffer.Length);
+            var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine(message);
+        }
+    }
+}
